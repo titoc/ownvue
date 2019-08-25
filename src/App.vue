cde3b376fab35b3bf4b493599b233691553e17f6
Tito Cheriachan <12772268+titoc@users.noreply.github.com>
2019-08-24 23:42:47 -0400

    Resume layout

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,33 @@
 <template>
-  <div id="app"
-    class="container-fluid">
+  <div id="app" class="container-fluid">
     <div class="row">
       <div class="col">
-        <b-navbar toggleable="lg"
-          type="dark"
-          variant="info">
+        <b-navbar toggleable="lg" type="dark" variant="info">
           <b-navbar-brand href="#">
-            <b-img left
+            <b-img
+              left
               rounded="circle"
               v-bind="mainProps"
               src="https://picsum.photos/250/250/?image=54"
-              alt="Circle image" />
+              alt="Circle image"
+            />
           </b-navbar-brand>
           <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse"
-            is-nav>
+          <b-collapse id="nav-collapse" is-nav>
             <b-navbar-nav>
-              <b-link to="home">Home</b-link>
+              <b-nav-item to="/">Home</b-nav-item>
               <b-nav-item to="/resume">Resume</b-nav-item>
-              <b-nav-item href="/sideprojects">Side Projects</b-nav-item>
-              <b-nav-item href="/about">About</b-nav-item>
-              <b-nav-item href="/contact">Contact</b-nav-item>
+              <b-nav-item to="/sideprojects">Side Projects</b-nav-item>
+              <b-nav-item to="/about">About</b-nav-item>
+              <b-nav-item to="/contact">Contact</b-nav-item>
             </b-navbar-nav>
           </b-collapse>
         </b-navbar>
 
-        <router-link to="/resume">Resume</router-link>|
+        <!-- <router-link to="/resume">Resume</router-link>|
         <router-link to="/sideprojects">Side Projects</router-link>|
         <router-link to="/about">About</router-link>|
-        <router-link to="/contact">Contact</router-link>|
+        <router-link to="/contact">Contact</router-link>|-->
       </div>
     </div>
     <div class="row">
@@ -43,10 +41,16 @@
 export default {
   data() {
     return {
-      mainProps: { blank: false, blankColor: '#777', width: 60, height: 60, class: 'm1' }
-    }
+      mainProps: {
+        blank: false,
+        blankColor: "#777",
+        width: 60,
+        height: 60,
+        class: "m1"
+      }
+    };
   }
-}
+};
 </script>
 <style lang="scss">
 </style>
--- src/components/WorkExperiences.vue
+++ src/components/WorkExperiences.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
-    <div v-for="experience in experiences"
-      v-bind:key="experience.id">
-      {{experience.id}} , {{experience.logo}} , {{experience.companyLink}} , {{experience.name}}
-      ,{{experience.designation}} , {{experience.from.year}} - {{experience.from.month}}
-      , {{experience.to.year}} - {{experience.to.month}}
-      <div v-for="skill in experience.skills"
-        v-bind:key="skill">
-        {{skill}}
-      </div>
-      <div v-for="activity in experience.activities"
-        v-bind:key="activity.id">
-        {{activity.id}},{{activity.activity}}
+    <div class="row" v-for="experience in experiences" v-bind:key="experience.id">
+      <div class="col">
+        <div class="mt-2 mb-2">
+          <b-card
+            border-variant="primary"
+            img-top
+            :title="experience.name"
+            :sub-title="experience.designation+` , `+experience.from.year+`/`+experience.from.month+` - `+experience.to.year+`/`+experience.to.month"
+          >
+            <b-card-text>
+              <b-img
+                left
+                rounded="circle"
+                src="https://picsum.photos/125/125/?image=58"
+                alt="Circle image"
+              />
+              ID: {{experience.id}}
+              Logo: {{experience.logo}}
+            </b-card-text>
+            <div v-for="activity in experience.activities" v-bind:key="activity.id">
+              <b-card-text>{{activity.id}},{{activity.activity}}</b-card-text>
+            </div>
+            <span v-for="skill in experience.skills" v-bind:key="skill">
+              <b-badge class="mr-2" variant="success">{{skill}}</b-badge>
+            </span>
+            <div>
+              <b-link :href="experience.companyLink" class="card-link">{{experience.companyLink}}</b-link>
+            </div>
+          </b-card>
+        </div>
       </div>
     </div>
   </div>
@@ -26,7 +44,6 @@ import EmployerService from "../services/EmployerService";
 export default class WorkExperiences extends Vue {
   experiences: Employer[] = [];
   service: EmployerService;
-
   constructor() {
     super();
     console.log("Im init");
