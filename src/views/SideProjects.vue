282f7b6d7b9a6a86eaea7238e2c433a29069289b
Tito <12772268+titoc@users.noreply.github.com>
2019-10-10 12:21:05 -0400

    Update SideProjects.vue

--- src/views/SideProjects.vue
+++ src/views/SideProjects.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <div class="row mt-2">
     <div class="col">
       <b-card title="OwnVue" sub-title="A handcoded personal website">
@@ -19,4 +20,25 @@
       </b-card>
     </div>
   </div>
+  <div class="row mt-2">
+    <div class="col">
+      <b-card title="DailyTweetAlerts" sub-title="A free web based service which sends alert emails when it finds particular keywords of your interests on twitter.">
+        <b-card-text>
+          This web application uses VueJS for the UI, Firebase for Twitter OAuth, Kafka and Spring Boot at the backend.
+          Kafka is used for processing a large number of tweets and for filtering out the irrelevant ones.
+          <b-link
+            href="https://www.dailytweetalerts.com"
+            class="card-link"
+            target="_blank"
+          >DailyTweetAlerts</b-link>
+        </b-card-text>
+        <b-badge class="mr-2" variant="success">VueJS</b-badge>
+        <b-badge class="mr-2" variant="success">SpringBoot</b-badge>
+        <b-badge class="mr-2" variant="success">Kafka</b-badge>
+        <b-badge class="mr-2" variant="success">Netlify</b-badge>
+        <b-badge class="mr-2" variant="success">Firebase</b-badge>
+      </b-card>
+    </div>
+  </div>
+ </div>
 </template>
